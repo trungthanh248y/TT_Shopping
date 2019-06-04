a95badcc88c02bc287fedbce8f73b24f82ab8dd4
Thanh <trungthanh248y@gmail.com>
2019-06-04 20:43:08 +0700

    make migration

---
+++ database/migrations/2019_06_04_130855_create_bills_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('payment')->nullable();
+            $table->string('status')->nullable();
+            $table->integer('total')->nullable();
+            $table->integer('id_user')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+}
---
+++ database/migrations/2019_06_04_131116_create_bill_detail_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBillDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bill_detail', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quantity')->nullable();
+            $table->string('id_bill')->nullable();
+            $table->string('id_product')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bill_detail');
+    }
+}
